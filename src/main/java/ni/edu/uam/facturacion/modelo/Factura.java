3b3b56462ee179049d3bd8a3adcc6ebc94789301
FranciscoGuevara9128 <fjguevara@uamv.edu.ni>
2025-11-03 17:49:36 -0600

    Lección 4: Refinar la interfaz de usuario

--- src/main/java/ni/edu/uam/facturacion/modelo/Factura.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Factura.java
@@ -11,6 +11,14 @@ import org.openxava.calculators.*;
 import lombok.*;
 
 @Entity @Getter @Setter
+
+@View(members= // Esta vista no tiene nombre, por tanto será la vista usada por defecto
+        "anyo, numero, fecha;" + // Separados por coma significa en la misma línea
+                "cliente;" + // Punto y coma significa nueva línea
+                "detalles;" +
+                "observaciones"
+)
+
 public class Factura {
 
     @Id
@@ -39,6 +47,7 @@ public class Factura {
     String observaciones;
 
     @ManyToOne(fetch=FetchType.LAZY, optional=false) // El cliente es obligatorio
+    @ReferenceView("Simple")
     Cliente cliente;
 
     @ElementCollection
